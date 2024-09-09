e2c3813cef85d3137c07f172676542e2bec1cb22
Valen <valentincanziani20@gmail.com>
2024-09-09 12:58:17 -0300

    SetpointHysteresis mod(Right store and index)

--- app/Http/Controllers/SetpointHysteresisController.php
+++ app/Http/Controllers/SetpointHysteresisController.php
@@ -7,9 +7,40 @@ use Illuminate\Http\Request;
 
 class SetpointHysteresisController extends Controller
 {
-    public function index()
+    public function index($sensorName)
     {
-        // Lógica para obtener los datos de la tabla
-        return SetpointHysteresis::all();
+        $setpoints = SetpointHysteresis::where('name', $sensorName)->get();
+
+        return response()->json($setpoints, 200);
+    }
+
+    public function store(Request $request, $sensorName)
+    {
+        $validatedData = $request->validate([
+            'upper_hyst' => 'required|integer',
+            'lower_hyst' => 'required|integer',
+        ]);
+
+        // Upper hysteresis must be greater than lower hysteresis
+        if ($validatedData['upper_hyst'] < $validatedData['lower_hyst']) {
+            return response()->json(['error' => 'upper_hyst cannot be less than lower_hyst.'], 422);
+        }
+
+        // The difference between upper_hyst and lower_hyst must be at least 4.
+        if (($validatedData['upper_hyst'] - $validatedData['lower_hyst']) < 4) {
+            return response()->json(['error' => 'The difference between upper_hyst and lower_hyst must be at least 4.'], 422);
+        }
+
+        // If the SetpointHysteresis already exists, update it. Otherwise, create a new one.
+        $setpoint = SetpointHysteresis::where('name', $sensorName)->first();
+
+        if ($setpoint) {
+            $setpoint->update($validatedData);
+        } else {
+            $validatedData['name'] = $sensorName;
+            $setpoint = SetpointHysteresis::create($validatedData);
+        }
+
+        return response()->json($setpoint, 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -25,4 +25,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->post('/{sensorName}/temperatures', [TemperatureController::class, 'store']);
-Route::middleware('auth:sanctum')->get('/setpoints', [SetpointHysteresisController::class, 'index']);
+
+Route::middleware('auth:sanctum')->get('/{sensorName}/setpoints', [SetpointHysteresisController::class, 'index']);
+Route::middleware('auth:sanctum')->post('/{sensorName}/setpoints', [SetpointHysteresisController::class, 'store']);

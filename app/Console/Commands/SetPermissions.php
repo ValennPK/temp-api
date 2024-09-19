1834429d0fa11889de929d010b3b54a6c40c20e2
Valen <valentincanziani20@gmail.com>
2024-09-19 17:31:49 -0300

    Set Permissions command created

---
+++ app/Console/Commands/SetPermissions.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\User;
+
+class SetPermissions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:set-permissions {username} {status}'; 
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set permissions for a specific thermometer.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $username = $this->argument('username');
+        $status = $this->argument('status');
+
+        $user = User::where('username', $username)->first();
+
+        if (!$user) {
+            $this->error('User not found.');
+            return;
+        }
+
+        $user->has_permission = $status;
+
+        $user->save();
+
+        $this->info('User permissions updated.');
+
+        return;
+    }
+}

1c9a34c17e35820da57b7de6968a2bcdb6431ddb
ValennPK <valentincanziani20@gmail.com>
2024-12-30 18:31:32 -0300

    syntax corrections

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Livewire\Auth\Register;
-use App\Livewire\Auth\Login;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\ThermometersController;
-use App\Models\Thermometer;
+use app\Livewire\ShowThermometer;
 
-Route::get('/register', Register::class)->name('register');
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-// Route::middleware('web')->group(function () {
-//     Route::view('/', 'livewire.index')->redirectTo('/login');
-// });
-
-Route::get('/', function () {
-    return redirect('/login');
-});
+Route::get('/', function () {return redirect('/login');});
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/thermometers/{ThermometerName}', function ($ThermometerName) {
-    return view('thermometers.index', compact('ThermometerName'));
-});
+Route::get('/thermometer/{thermometerName}', ShowThermometer::class);
+

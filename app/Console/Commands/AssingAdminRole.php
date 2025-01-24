33fdfbeaca2872251aa01f962ebb522f167d7de9
ValennPK <valentincanziani20@gmail.com>
2025-01-24 17:24:10 -0300

    AssingAdminRole class name changed from AssingAdmin to AssingAdminRole

--- app/Console/Commands/AssingAdminRole.php
+++ app/Console/Commands/AssingAdminRole.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\User;
 
-class AssingAdmin extends Command
+class AssingAdminRole extends Command
 {
     /**
      * The name and signature of the console command.

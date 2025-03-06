2122f96500b746c7b151f5a0f05ee4bb76952423
ValennPK <valentincanziani20@gmail.com>
2025-03-06 01:28:34 -0300

    debug test 1

--- app/Http/Controllers/TemperatureController.php
+++ app/Http/Controllers/TemperatureController.php
@@ -48,8 +48,8 @@ class TemperatureController extends Controller
 
     public function index_testigo($ThermometerName){
         $thermometer_id = Thermometer::where('username', $ThermometerName)->first()->id;
+        return $thermometer_id;
         $testigo_id = DB::table('thermometer_to_testigos')->where('thermometer_id', $thermometer_id)->first()->testigo_id;
-        // $testigo_id = ThermometerToTestigo::where('thermometer_id', $thermometer_id)->first()->testigo_id;
         $testigo_name = Thermometer::where('id', $testigo_id)->first()->username;
         $lastTemp = DB::table($testigo_name)->latest('created_at')->get("port1")->first();
 

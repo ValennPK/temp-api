15f8ea16976515322ee050d9b3bc0ec9f5ad9460
ValennPK <valentincanziani20@gmail.com>
2025-03-06 01:16:46 -0300

    ThermometerToTestigo model & mig modified

--- app/Models/ThermometerToTestigo.php
+++ app/Models/ThermometerToTestigo.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
-class ThermometerToTestigo  extends Model
+class ThermometerToTestigo extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'thermometer_to_testigo';
 
     protected $fillable = [
         'thermometer_id',
         'testigo_id',
     ];
+
+
 }

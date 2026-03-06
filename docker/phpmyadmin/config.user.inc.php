26805e8a2bede42fa20e4c281de45f5806aa5736
Gustavo del Pozo <eltanquedyr@yahoo.com.ar>
2026-03-06 12:54:13 +0000

    nueva configuracion en docker

--- docker/phpmyadmin/config.user.inc.php
+++ docker/phpmyadmin/config.user.inc.php
@@ -5,3 +5,10 @@ $cfg['PmaAbsoluteUri'] = 'http://localhost:8081/';
 $cfg['ForceSSL'] = false;
 $cfg['LoginCookieValidity'] = 36000;
 $cfg['CookieSameSite'] = 'Lax';
+
+// Keep phpMyAdmin aligned with Docker .env credentials to avoid auth mismatch.
+$cfg['Servers'][1]['auth_type'] = 'config';
+$cfg['Servers'][1]['host'] = getenv('PMA_HOST') ?: 'mysql';
+$cfg['Servers'][1]['port'] = (int) (getenv('PMA_PORT') ?: 3306);
+$cfg['Servers'][1]['user'] = getenv('PMA_USER') ?: 'sail';
+$cfg['Servers'][1]['password'] = getenv('PMA_PASSWORD') ?: 'password';
